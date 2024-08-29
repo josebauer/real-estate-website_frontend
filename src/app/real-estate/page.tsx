98cfee01130d29e02242863923b49ebca1232635
José Bauer <jose.h.bauer03@gmail.com>
2024-08-29 17:40:25 -0300

    Cria componente de filtro e adiciona página para listar e filtrar os imóveis

---
+++ src/app/real-estate/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import styles from "./page.module.scss"
+import SlideCard from "@/components/common/slideCard/SlideCard";
+import FilterForm from "@/components/realEstatePage/filterForm/FilterForm";
+import realEstateService, { FilterValues, RealEstateType } from "@/services/realEstateService";
+import { useEffect, useState } from "react";
+import { Button, Container } from "react-bootstrap";
+
+export default function RealEstate() {
+  const [filteredRealEstate, setFilteredRealEstate] = useState<RealEstateType[]>([])
+  const [filters, setFilters] = useState<FilterValues>({})
+  
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const savedFilters = localStorage.getItem('realEstateFilters')
+      if (savedFilters) {
+        const parsedFilters = JSON.parse(savedFilters)
+        setFilters(parsedFilters)
+        fetchRealEstate(parsedFilters)
+      } else {
+        fetchRealEstate()
+      }
+    }
+  }, [])
+
+  const fetchRealEstate = async (appliedFilters: FilterValues = { perPage: 12 }) => {
+    try {
+      const results = await realEstateService.getFilteredRealEstate(appliedFilters)
+      setFilteredRealEstate(results)
+  
+      if (typeof window !== "undefined") {
+        localStorage.setItem('realEstateResults', JSON.stringify(results))
+      }
+    } catch (error) {
+      console.error("Erro ao buscar imóveis:", error)
+    }
+  }
+
+  const handleFilter = (newFilters: FilterValues, results?: RealEstateType[]) => {
+    setFilters(newFilters)
+    if (results) {
+      setFilteredRealEstate(results)
+    } else {
+      fetchRealEstate(newFilters)
+    }
+  };
+
+  const clearFilters = () => {
+    setFilters({ perPage: 12 })
+    fetchRealEstate({ perPage: 12 } )
+    localStorage.removeItem('realEstateFilters')
+  }
+
+  return (
+    <>
+      <main>
+        <Container className="d-flex flex-column align-items-start py-5 gap-3">
+          <FilterForm filters={filters} onFilter={handleFilter} />
+          <Button className={styles.clearFiltersBtn} onClick={clearFilters}>Limpar Filtros</Button>
+          <div className={styles.filteredRealEstateList}>
+            {filteredRealEstate?.length > 0 ? (
+              filteredRealEstate.map(realEstate => (
+                <SlideCard key={realEstate.id} realEstate={realEstate} />
+              ))
+            ) : (
+              <p className={styles.realEstateNotFound}>Nenhum imóvel encontrado. Altere sua pesquisa!</p>
+            )}
+          </div>
+
+        </Container>
+      </main>
+    </>
+  )
+}
---
+++ src/components/realEstatePage/filterForm/FilterForm.tsx
@@ -0,0 +1,107 @@
+import styles from "./filterForm.module.scss";
+import { FormEvent, useEffect, useState } from 'react';
+import realEstateService, { FilterValues, RealEstateType } from '@/services/realEstateService';
+import categoriesService, { CategoryType } from "@/services/categoriesService";
+import { Form, FormSelect } from "react-bootstrap";
+
+interface FilterFormProps {
+  filters: FilterValues
+  onFilter: (filters: FilterValues, results: RealEstateType[]) => void
+}
+
+export default function FilterForm({ filters: initialFilters, onFilter }: FilterFormProps) {
+  const [filters, setFilters] = useState<FilterValues>(initialFilters)
+  const [categories, setCategories] = useState<CategoryType[]>([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const fetchedCategories = await categoriesService.getCategories()
+      setCategories(fetchedCategories)
+    }
+
+    fetchCategories()
+  }, [])
+
+
+  useEffect(() => {
+    setFilters(initialFilters)
+  }, [initialFilters])
+
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target
+    setFilters({
+      ...filters,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    const filteredRealEstate = await realEstateService.getFilteredRealEstate(filters)
+    onFilter(filters, filteredRealEstate)
+
+    setFilters({})
+  }
+
+  return (
+    <Form className={`${styles.filterForm} shadow`} onSubmit={handleSubmit}>
+      <FormSelect name="city" value={filters.city || ''} onChange={handleInputChange}>
+        <option value="">Cidade</option>
+        <option value="Canoinhas">Canoinhas</option>
+        <option value="Chapecó">Chapecó</option>
+      </FormSelect>
+
+      <FormSelect name="district" value={filters.district || ''} onChange={handleInputChange}>
+        <option value="">Bairro</option>
+        <option value="Alto da Tijuca">Alto da Tijuca</option>
+        <option value="Efapi">Efapi</option>
+        <option value="Centro">Centro</option>
+      </FormSelect>
+
+      <FormSelect name="categoryId" value={filters.categoryId || ''} onChange={handleInputChange}>
+        <option value="">Categoria</option>
+        {Array.isArray(categories) && categories.map((category) => (
+          <option
+            value={category.id}
+            key={category.id}
+          >
+            {category.name}
+          </option>
+        ))}
+      </FormSelect>
+
+      <FormSelect name="negotiation" value={filters.negotiation || ''} onChange={handleInputChange}>
+        <option value="">Finalidade</option>
+        <option value="venda">Venda</option>
+        <option value="locação">Locação</option>
+      </FormSelect>
+
+      <FormSelect name="bedrooms" value={filters.bedrooms || ''} onChange={handleInputChange}>
+        <option value="">Dormitórios</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4+</option>
+      </FormSelect>
+
+      <FormSelect name="garages" value={filters.garages || ''} onChange={handleInputChange}>
+        <option value="">Vagas</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4+</option>
+      </FormSelect>
+
+      <input
+        type="number"
+        name="id"
+        placeholder="# Código do Imóvel"
+        onChange={handleInputChange}
+        value={filters.id || ''}
+      />
+
+      <button className={styles.button} type="submit">Pesquisar</button>
+    </Form >
+  )
+}
